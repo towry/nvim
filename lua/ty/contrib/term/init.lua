631b6250f1b9923816fa74ecb08e99d8e0705783
towry <towry@users.noreply.github.com>
2023-03-16 13:40:24 +0800

    up

--- lua/ty/contrib/term/init.lua
+++ lua/ty/contrib/term/init.lua
@@ -6,9 +6,16 @@ M.init = function()
     callback = function()
       local terms = require("toggleterm.terminal").get_all()
       local is_shut = false
+      local job_ids = {}
+
       for _, term in ipairs(terms) do
-        term:shutdown()
+        table.insert(job_ids, term.job_id)
+        vim.fn.jobstop(term.job_id)
         is_shut = true
+      end
+
+      if #job_ids > 0 then
+        vim.fn.jobwait(job_ids, 2000)
       end
 
       if is_shut then
