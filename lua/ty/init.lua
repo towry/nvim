1e592760bd442f9fdd3ec85db6b89fe9bb929dd8
towry <towry@users.noreply.github.com>
2023-03-21 09:36:03 +0800

    Fix kitty padding

--- lua/ty/init.lua
+++ lua/ty/init.lua
@@ -5,6 +5,19 @@ local autocmd = require('ty.core.autocmd')
 local function setup_later_modules()
   require('ty.core.pack'):startup() -- start loading modules.
 end
+local function resize_kitty()
+  local kitty_aug = vim.api.nvim_create_augroup("kitty_aug", { clear = true })
+  vim.api.nvim_create_autocmd("VimEnter", {
+    group = kitty_aug,
+    pattern = "*",
+    command = ":silent !kitty @ --to=$KITTY_LISTEN_ON set-spacing padding=0 margin=0"
+  })
+  vim.api.nvim_create_autocmd("VimLeave", {
+    group = kitty_aug,
+    pattern = "*",
+    command = ":silent !kitty @ --to=$KITTY_LISTEN_ON set-spacing padding=8 margin=0"
+  })
+end
 
 function M.setup()
   if _inited then return end
@@ -13,6 +26,8 @@ function M.setup()
   vim.g.mapleader = ' '
   vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
   vim.g.maplocalleader = ','
+
+  resize_kitty()
 
   require('ty.core.globals')
   require('ty.core.options').setup()
