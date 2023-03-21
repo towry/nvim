1aea11dedb948a666d9246b7e4bbb0b5a1178ea9
towry <towry@users.noreply.github.com>
2023-03-21 15:31:18 +0800

    up

--- lua/ty/init.lua
+++ lua/ty/init.lua
@@ -7,7 +7,7 @@ local function resize_kitty()
     group = kitty_aug,
     pattern = '*',
     callback = function()
-      vim.defer_fn(function() vim.cmd(':silent !kitty @ --to=$KITTY_LISTEN_ON set-spacing padding=0 margin=0') end, 10)
+      vim.defer_fn(function() vim.cmd(':silent !kitty @ --to=$KITTY_LISTEN_ON set-spacing padding=0 margin=0') end, 1)
     end,
   })
   vim.api.nvim_create_autocmd('UILeave', {
