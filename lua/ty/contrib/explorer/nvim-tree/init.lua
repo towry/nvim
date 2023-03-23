992602f9b540051c22478e06fc5712cf3ea96a9d
towry <towry@users.noreply.github.com>
2023-03-23 10:06:39 +0800

    up

--- lua/ty/contrib/explorer/nvim-tree/init.lua
+++ lua/ty/contrib/explorer/nvim-tree/init.lua
@@ -66,12 +66,18 @@ local function tree_actions_menu(node)
 
       -- The following actions are disabled in this example
       -- You may want to map them too depending on your needs though
-      actions.add_selection:replace(function() end)
-      actions.remove_selection:replace(function() end)
-      actions.toggle_selection:replace(function() end)
-      actions.select_all:replace(function() end)
-      actions.drop_all:replace(function() end)
-      actions.toggle_all:replace(function() end)
+      actions.add_selection:replace(function()
+      end)
+      actions.remove_selection:replace(function()
+      end)
+      actions.toggle_selection:replace(function()
+      end)
+      actions.select_all:replace(function()
+      end)
+      actions.drop_all:replace(function()
+      end)
+      actions.toggle_all:replace(function()
+      end)
 
       return true
     end,
@@ -96,10 +102,9 @@ function M.setup()
   }
 
   local function enable_float_when_gui_narrow()
-    return true
-    -- local gwidth = vim.api.nvim_list_uis()[1].width
-    -- local gheight = vim.api.nvim_list_uis()[1].height
-    -- return gheight > gwidth
+    local gwidth = vim.api.nvim_list_uis()[1].width
+    local gheight = vim.api.nvim_list_uis()[1].height
+    return gheight > gwidth
   end
 
   local function get_float_center_metrix()
@@ -125,44 +130,44 @@ function M.setup()
     { key = { '<CR>', 'o', '<2-LeftMouse>' }, action = 'edit' },
     -- <C-e> keymapping cannot be set because it's used for toggling nvim-tree
     -- { key = "<C-e>",                        action = "edit_in_place" },
-    { key = { 'O' }, action = 'edit_no_picker' },
-    { key = { '<2-RightMouse>', '<C-]>' }, action = 'cd' },
-    { key = '<C-v>', action = 'vsplit' },
-    { key = '<C-x>', action = 'split' },
+    { key = { 'O' },                          action = 'edit_no_picker' },
+    { key = { '<2-RightMouse>', '<C-]>' },    action = 'cd' },
+    { key = '<C-v>',                          action = 'vsplit' },
+    { key = '<C-x>',                          action = 'split' },
     -- { key = "<C-t>",                          action = "tabnew" },
-    { key = '<', action = 'prev_sibling' },
-    { key = '>', action = 'next_sibling' },
-    { key = 'P', action = 'parent_node' },
-    { key = '<BS>', action = 'close_node' },
-    { key = '<Tab>', action = 'preview' },
-    { key = 'K', action = 'first_sibling' },
-    { key = 'J', action = 'last_sibling' },
-    { key = 'I', action = 'toggle_ignored' },
-    { key = 'H', action = 'toggle_dotfiles' },
-    { key = 'R', action = 'refresh' },
-    { key = 'a', action = 'create' },
-    { key = 'd', action = 'remove' },
-    { key = 'D', action = 'trash' },
-    { key = 'r', action = 'rename' },
-    { key = '<C-r>', action = 'full_rename' },
-    { key = 'x', action = 'cut' },
-    { key = 'c', action = 'copy' },
-    { key = 'p', action = 'paste' },
-    { key = 'y', action = 'copy_name' },
-    { key = 'Y', action = 'copy_path' },
-    { key = 'gy', action = 'copy_absolute_path' },
-    { key = '[c', action = 'prev_git_item' },
-    { key = ']c', action = 'next_git_item' },
-    { key = '-', action = 'dir_up' },
-    { key = 'X', action = 'system_open' },
-    { key = 'q', action = 'close' },
-    { key = '<Esc>', action = 'close' },
-    { key = 'g?', action = 'toggle_help' },
-    { key = 'W', action = 'collapse_all' },
+    { key = '<',                              action = 'prev_sibling' },
+    { key = '>',                              action = 'next_sibling' },
+    { key = 'P',                              action = 'parent_node' },
+    { key = '<BS>',                           action = 'close_node' },
+    { key = '<Tab>',                          action = 'preview' },
+    { key = 'K',                              action = 'first_sibling' },
+    { key = 'J',                              action = 'last_sibling' },
+    { key = 'I',                              action = 'toggle_ignored' },
+    { key = 'H',                              action = 'toggle_dotfiles' },
+    { key = 'R',                              action = 'refresh' },
+    { key = 'a',                              action = 'create' },
+    { key = 'd',                              action = 'remove' },
+    { key = 'D',                              action = 'trash' },
+    { key = 'r',                              action = 'rename' },
+    { key = '<C-r>',                          action = 'full_rename' },
+    { key = 'x',                              action = 'cut' },
+    { key = 'c',                              action = 'copy' },
+    { key = 'p',                              action = 'paste' },
+    { key = 'y',                              action = 'copy_name' },
+    { key = 'Y',                              action = 'copy_path' },
+    { key = 'gy',                             action = 'copy_absolute_path' },
+    { key = '[c',                             action = 'prev_git_item' },
+    { key = ']c',                             action = 'next_git_item' },
+    { key = '-',                              action = 'dir_up' },
+    { key = 'X',                              action = 'system_open' },
+    -- { key = 'q', action = 'close' },
+    { key = '<Esc>',                          action = 'close' },
+    { key = 'g?',                             action = 'toggle_help' },
+    { key = 'W',                              action = 'collapse_all' },
     -- { key = "/",                              action = "search_node" },
-    { key = 'f', action = 'live_filter' },
-    { key = 'F', action = 'clear_live_filter' },
-    { key = '<C-\\>', action = 'tree actions', action_cb = tree_actions_menu },
+    { key = 'f',                              action = 'live_filter' },
+    { key = 'F',                              action = 'clear_live_filter' },
+    { key = '<C-\\>',                         action = 'tree actions',      action_cb = tree_actions_menu },
   }
 
   require('nvim-tree').setup({
