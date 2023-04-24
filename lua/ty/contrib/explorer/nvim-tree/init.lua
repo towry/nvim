1adae51a4fb37dde9bf5ee7b11d8a39aa31a8a67
towry <towry@users.noreply.github.com>
2023-04-24 15:46:10 +0800

    migrate nvim tree api

--- lua/ty/contrib/explorer/nvim-tree/init.lua
+++ lua/ty/contrib/explorer/nvim-tree/init.lua
@@ -4,92 +4,7 @@ local HEIGHT_RATIO = 0.8 -- You can change this
 local WIDTH_RATIO = 0.5  -- You can change this too
 local TREE_INIT_WIDTH = 40
 
-local function tree_actions_menu(node)
-  local tree_actions = {
-    {
-      name = 'Create node',
-      handler = require('nvim-tree.api').fs.create,
-    },
-    {
-      name = 'Remove node',
-      handler = require('nvim-tree.api').fs.remove,
-    },
-    {
-      name = 'Trash node',
-      handler = require('nvim-tree.api').fs.trash,
-    },
-    {
-      name = 'Rename node',
-      handler = require('nvim-tree.api').fs.rename,
-    },
-    {
-      name = 'Fully rename node',
-      handler = require('nvim-tree.api').fs.rename_sub,
-    },
-    {
-      name = 'Copy',
-      handler = require('nvim-tree.api').fs.copy.node,
-    },
-    -- ... other custom actions you may want to display in the menu
-  }
-
-  local entry_maker = function(menu_item)
-    return {
-      value = menu_item,
-      ordinal = menu_item.name,
-      display = menu_item.name,
-    }
-  end
-
-  local finder = require('telescope.finders').new_table({
-    results = tree_actions,
-    entry_maker = entry_maker,
-  })
-
-  local sorter = require('telescope.sorters').get_generic_fuzzy_sorter()
-
-  local default_options = {
-    finder = finder,
-    sorter = sorter,
-    attach_mappings = function(prompt_buffer_number)
-      local actions = require('telescope.actions')
-
-      -- On item select
-      actions.select_default:replace(function()
-        local state = require('telescope.actions.state')
-        local selection = state.get_selected_entry()
-        -- Closing the picker
-        actions.close(prompt_buffer_number)
-        -- Executing the callback
-        selection.value.handler(node)
-      end)
-
-      -- The following actions are disabled in this example
-      -- You may want to map them too depending on your needs though
-      actions.add_selection:replace(function()
-      end)
-      actions.remove_selection:replace(function()
-      end)
-      actions.toggle_selection:replace(function()
-      end)
-      actions.select_all:replace(function()
-      end)
-      actions.drop_all:replace(function()
-      end)
-      actions.toggle_all:replace(function()
-      end)
-
-      return true
-    end,
-  }
-
-  -- Opening the menu
-  require('telescope.pickers').new({ prompt_title = 'Tree menu' }, default_options):find()
-end
-
 function M.setup()
-  local nvim_tree_api = require('nvim-tree.api')
-
   local git_icons = {
     unstaged = '',
     staged = '',
@@ -124,52 +39,8 @@ function M.setup()
     }
   end
 
-  -- TODO: https://github.com/nvim-tree/nvim-tree.lua/wiki/Migrating-To-on_attach
-  local keymappings = {
-    { key = { '<CR>', 'o', '<2-LeftMouse>' }, action = 'edit' },
-    -- <C-e> keymapping cannot be set because it's used for toggling nvim-tree
-    -- { key = "<C-e>",                        action = "edit_in_place" },
-    { key = { 'O' },                          action = 'edit_no_picker' },
-    { key = { '<2-RightMouse>', '<C-]>' },    action = 'cd' },
-    { key = '<C-v>',                          action = 'vsplit' },
-    { key = '<C-x>',                          action = 'split' },
-    -- { key = "<C-t>",                          action = "tabnew" },
-    { key = '<',                              action = 'prev_sibling' },
-    { key = '>',                              action = 'next_sibling' },
-    { key = 'P',                              action = 'parent_node' },
-    { key = '<BS>',                           action = 'close_node' },
-    { key = '<Tab>',                          action = 'preview' },
-    { key = 'K',                              action = 'first_sibling' },
-    { key = 'J',                              action = 'last_sibling' },
-    { key = 'I',                              action = 'toggle_ignored' },
-    { key = 'H',                              action = 'toggle_dotfiles' },
-    { key = 'R',                              action = 'refresh' },
-    { key = 'a',                              action = 'create' },
-    { key = 'd',                              action = 'remove' },
-    { key = 'D',                              action = 'trash' },
-    { key = 'r',                              action = 'rename' },
-    { key = '<C-r>',                          action = 'full_rename' },
-    { key = 'x',                              action = 'cut' },
-    { key = 'c',                              action = 'copy' },
-    { key = 'p',                              action = 'paste' },
-    { key = 'y',                              action = 'copy_name' },
-    { key = 'Y',                              action = 'copy_path' },
-    { key = 'gy',                             action = 'copy_absolute_path' },
-    { key = '[c',                             action = 'prev_git_item' },
-    { key = ']c',                             action = 'next_git_item' },
-    { key = '-',                              action = 'dir_up' },
-    { key = 'X',                              action = 'system_open' },
-    -- { key = 'q', action = 'close' },
-    { key = '<Esc>',                          action = 'close' },
-    { key = 'g?',                             action = 'toggle_help' },
-    { key = 'W',                              action = 'collapse_all' },
-    -- { key = "/",                              action = "search_node" },
-    { key = 'f',                              action = 'live_filter' },
-    { key = 'F',                              action = 'clear_live_filter' },
-    { key = '<C-m>',                          action = 'tree actions',      action_cb = tree_actions_menu },
-  }
-
   require('nvim-tree').setup({
+    on_attach = require('ty.contrib.explorer.nvim-tree.attach').on_attach,
     -- disables netrw completely
     disable_netrw = true,
     -- hijack netrw window on startup
@@ -272,13 +143,6 @@ function M.setup()
       hide_root_folder = false,
       -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
       side = 'left',
-      mappings = {
-        -- custom only false will merge the list with the default mappings
-        -- if true, it will only use your list to set the mappings
-        custom_only = true,
-        -- list of mappings to set on the tree manually
-        list = keymappings,
-      },
       number = false,
       relativenumber = true,
       centralize_selection = true,
@@ -308,22 +172,6 @@ function M.setup()
       always_show_folders = true,
     },
   })
-
-  -- edit file on file creation from nvim tree.
-  nvim_tree_api.events.subscribe(
-    nvim_tree_api.events.Event.FileCreated,
-    function(file)
-      -- FIXME: check window count.
-      vim.cmd('edit ' .. file.fname)
-    end
-  )
-  -- nvim_tree_events.on_tree_open(function()
-  --   bufferline_api.set_offset(TREE_WIDTH + 1, utils.add_whitespaces(13) .. 'File Explorer')
-  -- end)
-  --
-  -- nvim_tree_events.on_tree_close(function()
-  --   bufferline_api.set_offset(0)
-  -- end)
 end
 
 local function run_nvim_tree_toggle_cmd(cmd)
