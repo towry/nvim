7fdd4f4e567a00fd55d69ccfa3a0006bb4512211
towry <towry@users.noreply.github.com>
2023-05-04 16:32:17 +0800

    change term winblend

--- lua/ty/contrib/term/package_rc.lua
+++ lua/ty/contrib/term/package_rc.lua
@@ -30,15 +30,15 @@ M.setup_toggleterm = function()
     },
     shade_filetypes = { 'none' },
     shade_terminals = true,
-    shading_factor = 1, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+    shading_factor = 1,     -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
     start_in_insert = true,
     insert_mappings = true, -- whether or not the open mapping applies in insert mode
     persist_size = false,
     persist_mode = false,
     auto_scroll = false,
     direction = 'horizontal', -- | 'horizontal' | 'window' | 'float',
-    close_on_exit = true, -- close the terminal window when the process exits
-    shell = vim.o.shell, -- change the default shell
+    close_on_exit = true,     -- close the terminal window when the process exits
+    shell = vim.o.shell,      -- change the default shell
     -- This field is only relevant if direction is set to 'float'
     float_opts = {
       -- The border key is *almost* the same as 'nvim_win_open'
@@ -46,7 +46,7 @@ M.setup_toggleterm = function()
       -- the 'curved' border is a custom border type
       -- not natively supported but implemented in this plugin.
       border = 'double', -- single/double/shadow/curved
-      winblend = 0,
+      winblend = 15,
     },
     winbar = {
       enabled = false,
