3faae1a7d507f2c219dc6566972726848ec71ceb
towry <towry@users.noreply.github.com>
2023-06-06 17:53:49 +0800

    up

--- lua/user/config/init.lua
+++ lua/user/config/init.lua
@@ -1,12 +1,13 @@
 local M = {}
 
 function M.setup()
-  require('user.config.custom').setup()
-  require('user.config.options').setup()
-  require('plugins').setup()
-  require('user.config.autocmd').setup({
-    resize_kitty = true
-  })
+    require('user.config.custom').setup()
+    require('user.config.options').setup()
+    require('user.config.keymaps').setup()
+    require('plugins').setup()
+    require('user.config.autocmd').setup({
+        resize_kitty = true
+    })
 end
 
 return M
