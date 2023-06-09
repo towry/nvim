8e267097a7b2c3b5b037ef176480d1214bc8245f
towry <towry@users.noreply.github.com>
2023-06-09 22:00:52 +0800

    Add auto size window

---
+++ lua/plugins/workbench/workflow/autosize-window.lua
@@ -0,0 +1,17 @@
+return {
+  'anuvyklack/windows.nvim',
+  dependencies = {
+    'anuvyklack/middleclass',
+  },
+  opts = {},
+  lazy = true,
+  cmd = {
+    'WindowsMaximize',
+    'WindowsMaximizeVertically',
+    'WindowsMaximizeHorizontally',
+    'WindowsEqualize',
+    'WindowsEnableAutowidth',
+    'WindowsDisableAutowidth',
+    'WindowsToggleAutowidth',
+  }
+}
