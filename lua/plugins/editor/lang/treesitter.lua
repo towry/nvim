02d324235bb17ced2a5c7da25d0454479143fea6
towry <towry@users.noreply.github.com>
2023-06-09 15:20:54 +0800

    up

--- lua/plugins/editor/lang/treesitter.lua
+++ lua/plugins/editor/lang/treesitter.lua
@@ -121,7 +121,7 @@ function M.config()
         -- Set to false if you have an `updatetime` of ~100.
         clear_on_cursor_move = true,
       },
-      highlight_current_scope = { enable = true },
+      highlight_current_scope = { enable = false },
       smart_rename = {
         enable = false,
         keymaps = {
