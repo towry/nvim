575f129bea6d68fb4c09a74e1458cd761a592cec
towry <towry@users.noreply.github.com>
2023-06-12 14:10:02 +0800

    up

--- lua/plugins/git/diffview.lua
+++ lua/plugins/git/diffview.lua
@@ -13,28 +13,33 @@ return {
     'DiffviewFileHistory',
     'DiffviewToggleFiles',
   },
-  config = function()
-    require('diffview').setup({
-      view = {
-        default = {
-          layout = "diff2_vertical",
-          winbar_info = false,
-        },
+  opts = {
+    enhanced_diff_hl = true,
+    view = {
+      default = {
+        layout = "diff2_vertical",
+        winbar_info = false,
       },
-      hooks = {
-        diff_buf_read = function(bufnr)
-          local autocmd = require('libs.runtime.au')
-          autocmd.fire_event(autocmd.events.onGitDiffviewBufRead, {
-            bufnr = bufnr,
-          })
-        end,
-        view_opened = function(view)
-          local autocmd = require('libs.runtime.au')
-          autocmd.fire_event(autocmd.events.onGitDiffviewOpen, {
-            view = view,
-          })
-        end
-      }
-    })
+    },
+    keymaps = {
+      disable_defaults = false,
+    },
+    hooks = {
+      diff_buf_read = function(bufnr)
+        local autocmd = require('libs.runtime.au')
+        autocmd.fire_event(autocmd.events.onGitDiffviewBufRead, {
+          bufnr = bufnr,
+        })
+      end,
+      view_opened = function(view)
+        local autocmd = require('libs.runtime.au')
+        autocmd.fire_event(autocmd.events.onGitDiffviewOpen, {
+          view = view,
+        })
+      end
+    }
+  },
+  config = function(_, opts)
+    require('diffview').setup(opts)
   end,
 }
