58ddf312d04695527f29cc2c465e5d459cc0b62e
towry <towry@users.noreply.github.com>
2023-06-16 10:59:18 +0800

    up

--- lua/user/plugins/autocmp.lua
+++ lua/user/plugins/autocmp.lua
@@ -99,11 +99,13 @@ pack.plug({
       local buffer_option = {
         -- Complete from all visible buffers (splits)
         get_bufnrs = function()
-          local bufs = {}
-          for _, win in ipairs(vim.api.nvim_list_wins()) do
-            bufs[vim.api.nvim_win_get_buf(win)] = true
-          end
-          return vim.tbl_keys(bufs)
+          --- from all loaded buffers
+          return vim.api.nvim_list_bufs()
+          -- local bufs = {}
+          -- for _, win in ipairs(vim.api.nvim_list_wins()) do
+          --   bufs[vim.api.nvim_win_get_buf(win)] = true
+          -- end
+          -- return vim.tbl_keys(bufs)
         end,
       }
 
