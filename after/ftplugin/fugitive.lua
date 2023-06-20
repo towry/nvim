e76abff7032648ee70da0e03191824c274b80b8c
towry <towry@users.noreply.github.com>
2023-06-20 12:14:24 +0800

    auto jump in fug ft

--- after/ftplugin/fugitive.lua
+++ after/ftplugin/fugitive.lua
@@ -1,1 +1,9 @@
 vim.api.nvim_buf_set_keymap(0, 'n', 'q', ':bd<cr>', { nowait = true, noremap = true, silent = true })
+
+vim.schedule(function()
+  local ft = vim.bo.filetype
+  if ft ~= 'fugitive' then
+    return
+  end
+  vim.cmd('normal! 5j')
+end)
