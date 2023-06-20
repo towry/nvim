6fd3b3317fc5ce2aba82eb8cd71ad39e15d473e5
towry <towry@users.noreply.github.com>
2023-06-20 16:47:23 +0800

    change color

--- lua/user/plugins/theme.lua
+++ lua/user/plugins/theme.lua
@@ -48,7 +48,7 @@ plug({
       vim.g.everforest_disable_italic_comment = false
       vim.g.everforest_transparent_background = false
       vim.g.everforest_dim_inactive_windows = false
-      vim.g.everforest_sign_column_background = 'none' -- "none" | "grey"
+      vim.g.everforest_sign_column_background = 'none'  -- "none" | "grey"
       vim.g.everforest_diagnostic_virtual_text = 'grey' -- "grey" | "colored"
       vim.g.everforest_diagnostic_text_highlight = 0
       vim.g.everforest_diagnostic_line_highlight = 0
@@ -63,7 +63,14 @@ plug({
   },
   lazy = not string.match(vim.cfg.ui__theme_name, 'bones'),
   priority = 1000,
-  config = false
+  config = false,
+  init = function()
+    vim.g.forestbones = {
+      -- solid_line_nr = true,
+      darken_comments = 45,
+      solid_float_border = true,
+    }
+  end,
 })
 
 plug({
