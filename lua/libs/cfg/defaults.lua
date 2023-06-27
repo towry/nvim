9f05f833c81df46b271f22995ad921c9efa255c3
towry <towry@users.noreply.github.com>
2023-06-27 18:16:45 +0800

    up

--- lua/libs/cfg/defaults.lua
+++ lua/libs/cfg/defaults.lua
@@ -55,13 +55,19 @@ return {
   },
   editorExtend__colorizer_enable_tailwind_color = true,
   ---languages
+  ---- must installed, otherwise slowness.
   lang__treesitter_ensure_installed = {
+    'comment',
+    'diff',
+    'vim',
+    'vimdoc',
     'bash',
     'css',
     'html',
     'javascript',
     'json',
     'jsonc',
+    'jsdoc',
     'lua',
     'python',
     'regex',
