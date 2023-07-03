cf6153f061afe9ce96831713d2edf30a96b10c25
towry <towry@users.noreply.github.com>
2023-07-03 16:57:34 +0800

    fix toggleterm kill

--- lua/libs/terminal/toggleterm_kill_all.lua
+++ lua/libs/terminal/toggleterm_kill_all.lua
@@ -5,6 +5,11 @@ local function toggleterm_kill_all()
   local job_ids = {}
 
   for _, term in ipairs(terms) do
+    --- some hack,
+    --- otherwise toggleterm will try to delete buffer, close window etc, we do not want it do this at vim exit.
+    term.on_exit = nil
+    term.close_on_exit = false
+
     local ret = vim.fn.jobstop(term.job_id)
     if ret == 1 then
       table.insert(job_ids, term.job_id)
