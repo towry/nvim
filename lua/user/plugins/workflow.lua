92a3f33cdcd881ad5ef0f70f966186105de872b9
towry <towry@users.noreply.github.com>
2023-07-03 10:41:05 +0800

    up

--- lua/user/plugins/workflow.lua
+++ lua/user/plugins/workflow.lua
@@ -366,7 +366,7 @@ plug({
           show_hidden = false,
           -- When set to false, you will get a message when project.nvim changes your
           -- directory.
-          silent_chdir = true,
+          silent_chdir = false,
           -- What scope to change the directory, valid options are
           -- * global (default)
           -- * tab
