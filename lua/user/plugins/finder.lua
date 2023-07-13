8e6187bd83f039c61aad5315608a395c0ed42ba4
towry <towry@users.noreply.github.com>
2023-07-13 20:10:07 +0800

    up

--- lua/user/plugins/finder.lua
+++ lua/user/plugins/finder.lua
@@ -34,7 +34,7 @@ plug({
   },
   config = function()
     local HEIGHT_RATIO = 0.8 -- You can change this
-    local WIDTH_RATIO = 0.5  -- You can change this too
+    local WIDTH_RATIO = 0.5 -- You can change this too
     local TREE_INIT_WIDTH = 40
 
 
@@ -356,7 +356,7 @@ plug({
 plug({
   'simrat39/symbols-outline.nvim',
   keys = {
-    { '<leader>/o',  '<cmd>SymbolsOutline<cr>', desc = 'Symbols outline' },
+    { '<leader>/o', '<cmd>SymbolsOutline<cr>', desc = 'Symbols outline' },
     -- <CMD-o> open the outline.
     { '<Char-0xAF>', '<cmd>SymbolsOutline<cr>', desc = 'Symbols outline' },
   },
@@ -587,6 +587,7 @@ plug({
     },
     {
       'pze/telescope-file-browser.nvim',
+      branch = 'feat/max-results',
       dev = false,
     },
   },
