8cdf69d4e430f979ea92ba3d4fa4708c8ba7a612
towry <towry@users.noreply.github.com>
2023-07-13 16:49:04 +0800

    disable eslint_d, because not working correctly in monorepo

--- lua/userlib/lspconfig-servers/null_ls/init.lua
+++ lua/userlib/lspconfig-servers/null_ls/init.lua
@@ -16,8 +16,8 @@ return function()
     -- builtins.code_actions.gitsigns,
     -- require("typescript.extensions.null-ls.code-actions"), -- disabled on volar take over mode.
     -- eslint.
-    builtins.code_actions.eslint_d,
-    builtins.diagnostics.eslint_d,
+    builtins.code_actions.eslint,
+    builtins.diagnostics.eslint,
   }
 
   nls.setup({
