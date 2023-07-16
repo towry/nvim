8da7bcadcdb51d298ede326b0d416d85328c0a53
towry <towry@users.noreply.github.com>
2023-07-16 23:31:27 +0800

    up

--- after/ftplugin/Outline.lua
+++ after/ftplugin/Outline.lua
@@ -4,13 +4,13 @@ vim.cmd([[setlocal signcolumn=no]])
 vim.cmd([[setlocal nohlsearch]])
 vim.cmd([[setlocal statuscolumn=]])
 
-local gr = vim.api.nvim_create_augroup('outline_exit', { clear = true })
-vim.api.nvim_create_autocmd('BufWinLeave', {
-  group = gr,
-  buffer = 0,
-  callback = function()
-    vim.schedule(function()
-      vim.cmd('SymbolsOutlineClose')
-    end)
-  end,
-})
+-- local gr = vim.api.nvim_create_augroup('outline_exit', { clear = true })
+-- vim.api.nvim_create_autocmd('BufWinLeave', {
+--   group = gr,
+--   buffer = 0,
+--   callback = function()
+--     vim.schedule(function()
+--       vim.cmd('SymbolsOutlineClose')
+--     end)
+--   end,
+-- })
