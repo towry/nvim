762f6955c8d9aa36551174a849771520d53a5ddc
towry <towry@users.noreply.github.com>
2023-07-18 11:31:25 +0800

    disable highlight for debug

--- lua/user/plugins/lang.lua
+++ lua/user/plugins/lang.lua
@@ -16,7 +16,7 @@ plug({
     keys = {
       -- { "<Enter>",    desc = "Init Increment selection" },
       -- { "<Enter>",    desc = "node node incremental selection",      mode = "x" },
-      { "<BS>", desc = "Decrement selection", mode = "x" },
+      { "<BS>",       desc = "Decrement selection",                  mode = "x" },
       { '<leader>cr', desc = 'Smart rename/nvim-treesitter-refactor' },
     },
     dependencies = {
@@ -90,12 +90,13 @@ plug({
       require('nvim-treesitter.configs').setup({
         -- parser_install_dir = parser_install_dir,
         ensure_installed = vim.cfg.lang__treesitter_ensure_installed, -- one of "all", or a list of languages
-        sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+        sync_install = false,                                         -- install languages synchronously (only applied to `ensure_installed`)
         auto_install = false,
-        ignore_install = { 'all' }, -- list of parsers to ignore installing
+        ignore_install = { 'all' },                                   -- list of parsers to ignore installing
         highlight = {
-          disable = disabled,
-          enable = vim.cfg.lang__treesitter_plugin_highlight,
+          -- disable = disabled,
+          -- enable = vim.cfg.lang__treesitter_plugin_highlight,
+          enable = false,
           -- disable = { "c", "rust" },  -- list of language that will be disabled
           additional_vim_regex_highlighting = false,
         },
@@ -337,7 +338,7 @@ plug({
       local todo_comments = require('todo-comments')
 
       todo_comments.setup({
-        signs = false, -- show icons in the signs column
+        signs = false,     -- show icons in the signs column
         sign_priority = 8, -- sign priority
         -- keywords recognized as todo comments
         keywords = {
@@ -348,14 +349,14 @@ plug({
           PERF = { alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
         },
         highlight = {
-          before = '', -- "fg" or "bg" or empty
+          before = '',                     -- "fg" or "bg" or empty
           -- keyword = "wide", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
-          keyword = 'wide', -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
-          after = '', -- "fg" or "bg" or empty
+          keyword = 'wide',                -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
+          after = '',                      -- "fg" or "bg" or empty
           pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
-          comments_only = true, -- uses treesitter to match keywords in comments only
-          max_line_len = 1500, -- ignore lines longer than this
-          exclude = {}, -- list of file types to exclude highlighting
+          comments_only = true,            -- uses treesitter to match keywords in comments only
+          max_line_len = 1500,             -- ignore lines longer than this
+          exclude = {},                    -- list of file types to exclude highlighting
         },
       })
     end,
@@ -384,17 +385,17 @@ plug({
       require('package-info').setup({
         colors = {
           up_to_date = '#3C4048', -- Text color for up to date package virtual text
-          outdated = '#fc514e', -- Text color for outdated package virtual text
+          outdated = '#fc514e',   -- Text color for outdated package virtual text
         },
         icons = {
-          enable = true, -- Whether to display icons
+          enable = true,                    -- Whether to display icons
           style = {
             up_to_date = icons.checkSquare, -- Icon for up to date packages
-            outdated = icons.gitRemove, -- Icon for outdated packages
+            outdated = icons.gitRemove,     -- Icon for outdated packages
           },
         },
-        autostart = true, -- Whether to autostart when `package.json` is opened
-        hide_up_to_date = true, -- It hides up to date versions when displaying virtual text
+        autostart = true,              -- Whether to autostart when `package.json` is opened
+        hide_up_to_date = true,        -- It hides up to date versions when displaying virtual text
         hide_unstable_versions = true, -- It hides unstable versions from version list e.g next-11.1.3-canary3
         -- Can be `npm` or `yarn`. Used for `delete`, `install` etc...
         -- The plugin will try to auto-detect the package manager based on
