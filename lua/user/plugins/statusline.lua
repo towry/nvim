d5e510d1ef89db49ad38a6ae35e77e1091ca9687
towry <towry@users.noreply.github.com>
2023-07-22 20:37:06 +0800

    up

--- lua/user/plugins/statusline.lua
+++ lua/user/plugins/statusline.lua
@@ -292,16 +292,16 @@ plug({
           },
           { text = { builtin.lnumfunc, ' ' }, click = 'v:lua.ScLa' },
           { text = { builtin.foldfunc, '' }, click = 'v:lua.ScFa' },
-          -- {
-          --   sign = { name = { 'GitSigns' }, maxwidth = 1, colwidth = 1, auto = true },
-          --   click = 'v:lua.ScSa',
-          -- },
           {
             sign = { name = { 'Diagnostic' }, maxwidth = 1, auto = false },
             click = 'v:lua.ScSa',
           },
           {
             sign = { namespace = { '.*' }, maxwidth = 2, colwidth = 2, auto = true },
+          },
+          {
+            sign = { name = { 'GitSigns' }, maxwidth = 1, colwidth = 1, auto = false },
+            click = 'v:lua.ScSa',
           },
         },
       })
