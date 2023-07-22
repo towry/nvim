10f81c47c3b97d4cacea7a63f71690616e653bb4
towry <towry@users.noreply.github.com>
2023-07-22 11:59:43 +0800

    remove plugsin

--- lua/user/plugins/workflow.lua
+++ lua/user/plugins/workflow.lua
@@ -118,8 +118,8 @@ plug({
           if buftype ~= "" then
             require('mini.bufremove').wipeout(0)
             if not vim.tbl_contains({
-                  'terminal',
-                }, buftype) then
+              'terminal',
+            }, buftype) then
               vim.cmd('q')
             end
             return
@@ -152,94 +152,6 @@ plug({
       lastplace_ignore_filetype = vim.cfg.misc__ft_exclude,
       lastplace_open_folds = true,
     }
-  },
-
-  --- auto close buffer after a time.
-  {
-    'chrisgrieser/nvim-early-retirement',
-    enabled = false,
-    config = function()
-      require('early-retirement').setup({
-        retirementAgeMins = 15,
-        ignoreAltFile = true,
-        minimumBufferNum = 10,
-        ignoreUnsavedChangesBufs = true,
-        ignoreSpecialBuftypes = true,
-        ignoreVisibleBufs = true,
-        ignoreUnloadedBufs = false,
-        notificationOnAutoClose = true,
-      })
-    end,
-    init = function()
-      local loaded = false
-      au.define_autocmds({
-        {
-          "User",
-          {
-            group = "_plugin_load_early_retirement",
-            pattern = au.user_autocmds.FileOpened,
-            once = true,
-            callback = function()
-              if loaded then
-                return
-              end
-              loaded = true
-              vim.defer_fn(function()
-                vim.cmd("Lazy load nvim-early-retirement")
-              end, 2000)
-            end,
-          }
-        }
-      })
-    end,
-  },
-
-  ----- file
-  {
-    -- Convenience file operations for neovim, written in lua.
-    "chrisgrieser/nvim-genghis",
-    init = function()
-      require('userlib.legendary').pre_hook('setup_lg_genghis_files_ops', function(lg)
-        local genghis = require('genghis')
-
-        lg.funcs({
-          {
-            description = 'File: Copy file path',
-            genghis.copyFilepath,
-          },
-          {
-            description = 'File: Change file mode',
-            genghis.chmodx,
-          },
-          {
-            description = 'File: Rename file',
-            genghis.renameFile,
-          },
-          {
-            description = 'File: Move and rename file',
-            genghis.moveAndRenameFile,
-          },
-          {
-            description = 'File: Create new file',
-            genghis.createNewFile,
-          },
-          {
-            description = 'File: Duplicate file',
-            genghis.duplicateFile,
-          },
-          {
-            description = 'File: Trash file',
-            function()
-              genghis.trashFile()
-            end,
-          },
-          {
-            description = 'File: Move selection to new file',
-            genghis.moveSelectionToNewFile,
-          }
-        })
-      end)
-    end
   },
 
   ----- grapple and portal
@@ -314,8 +226,8 @@ plug({
     'cbochs/grapple.nvim',
     keys = {
       { '<leader>bg', '<cmd>GrappleToggle<cr>', desc = 'Toggle grapple' },
-      { '<leader>bp', '<cmd>GrapplePopup<cr>',  desc = 'Popup grapple' },
-      { '<leader>bc', '<cmd>GrappleCycle<cr>',  desc = 'Cycle grapple' },
+      { '<leader>bp', '<cmd>GrapplePopup<cr>', desc = 'Popup grapple' },
+      { '<leader>bc', '<cmd>GrappleCycle<cr>', desc = 'Cycle grapple' },
     },
     cmd = { 'GrappleToggle', 'GrapplePopup', 'GrappleCycle' },
     opts = {
@@ -326,26 +238,6 @@ plug({
       },
     }
   },
-  ---- monorepo
-  {
-    "imNel/monorepo.nvim",
-    keys = {
-      {
-        '<leader>fm',
-        [[<cmd>lua require("telescope").extensions.monorepo.monorepo()<cr>]],
-        desc = 'Manage monorepo',
-      },
-      {
-        '<leader>f$',
-        [[<cmd>lua require("monorepo").toggle_project()<cr>]],
-        desc = 'Toggle cwd as project'
-      },
-    },
-    opts = {
-      autoload_telescope = true,
-    }
-  },
-
   {
     'telescope.nvim',
     dependencies = {
@@ -447,39 +339,35 @@ plug({
       local Path = require('plenary.path')
 
       session_manager.setup({
-        sessions_dir = Path:new(vim.fn.stdpath('data'), 'sessions'),             -- The directory where the session files will be saved.
-        path_replacer = '__',                                                    -- The character to which the path separator will be replaced for session files.
-        colon_replacer = '++',                                                   -- The character to which the colon symbol will be replaced for session files.
+        sessions_dir = Path:new(vim.fn.stdpath('data'), 'sessions'), -- The directory where the session files will be saved.
+        path_replacer = '__', -- The character to which the path separator will be replaced for session files.
+        colon_replacer = '++', -- The character to which the colon symbol will be replaced for session files.
         autoload_mode = require('session_manager.config').AutoloadMode.Disabled, -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
-        autosave_last_session = true,                                            -- Automatically save last session on exit and on session switch.
-        autosave_ignore_not_normal = true,                                       -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
+        autosave_last_session = true, -- Automatically save last session on exit and on session switch.
+        autosave_ignore_not_normal = true, -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
         autosave_ignore_filetypes = vim.tbl_extend('force',
-          {                                                                      -- All buffers of these file types will be closed before the session is saved.
+          { -- All buffers of these file types will be closed before the session is saved.
             'gitcommit',
             'toggleterm',
             'term',
             'nvimtree'
           }, vim.cfg.misc__ft_exclude),
         autosave_only_in_session = false, -- Always autosaves session. If true, only autosaves after a session is active.
-        max_path_length = 0,              -- Shorten the display path if length exceeds this threshold. Use 0 if don't want to shorten the path at all.
+        max_path_length = 0, -- Shorten the display path if length exceeds this threshold. Use 0 if don't want to shorten the path at all.
       })
     end,
   },
 
   {
-    'kwkarlwang/bufresize.nvim',
-    config = true,
-  },
-  {
     'mrjones2014/smart-splits.nvim',
     keys = {
-      { '<A-h>', cmdstr([[lua require("smart-splits").resize_left()]]),       desc = 'Resize window to left' },
-      { '<A-j>', cmdstr([[lua require("smart-splits").resize_down()]]),       desc = 'Resize window to down' },
-      { '<A-k>', cmdstr([[lua require("smart-splits").resize_up()]]),         desc = 'Resize window to up' },
-      { '<A-l>', cmdstr([[lua require("smart-splits").resize_right()]]),      desc = 'Resize window to right' },
-      { '<C-h>', cmdstr([[lua require("smart-splits").move_cursor_left()]]),  desc = 'Move cursor to left window' },
-      { '<C-j>', cmdstr([[lua require("smart-splits").move_cursor_down()]]),  desc = 'Move cursor to down window' },
-      { '<C-k>', cmdstr([[lua require("smart-splits").move_cursor_up()]]),    desc = 'Move cursor to up window' },
+      { '<A-h>', cmdstr([[lua require("smart-splits").resize_left()]]), desc = 'Resize window to left' },
+      { '<A-j>', cmdstr([[lua require("smart-splits").resize_down()]]), desc = 'Resize window to down' },
+      { '<A-k>', cmdstr([[lua require("smart-splits").resize_up()]]), desc = 'Resize window to up' },
+      { '<A-l>', cmdstr([[lua require("smart-splits").resize_right()]]), desc = 'Resize window to right' },
+      { '<C-h>', cmdstr([[lua require("smart-splits").move_cursor_left()]]), desc = 'Move cursor to left window' },
+      { '<C-j>', cmdstr([[lua require("smart-splits").move_cursor_down()]]), desc = 'Move cursor to down window' },
+      { '<C-k>', cmdstr([[lua require("smart-splits").move_cursor_up()]]), desc = 'Move cursor to up window' },
       { '<C-l>', cmdstr([[lua require("smart-splits").move_cursor_right()]]), desc = 'Move cursor to right window' },
     },
     dependencies = {
@@ -515,102 +403,6 @@ plug({
         log_level = "error",
         disable_multiplexer_nav_when_zoomed = true,
       })
-    end,
-  },
-
-  {
-    'folke/trouble.nvim',
-    cmd = { 'TroubleToggle', 'Trouble' },
-    keys = {
-      {
-        '<leader>cd', '<cmd>TroubleToggle document_diagnostics<cr>', desc = 'Toggle document diagnostics'
-      },
-      {
-        '<leader>wd', '<cmd>TroubleToggle workspace_diagnostics<cr>', desc = 'Toggle workspace diagnostics'
-      },
-      {
-        '<leader>tq',
-        function()
-          if require('trouble').is_open() then
-            require('trouble').close()
-            return
-          end
-          require('userlib.runtime.qf').toggle_qf()
-        end,
-        desc = 'Toggle Quickfix'
-      },
-      {
-        '<leader>tl',
-        function()
-          require('userlib.runtime.qf').toggle_loc()
-        end,
-        desc = 'Toggle loclist',
-      }
-    },
-    config = function()
-      local icons = require('userlib.icons')
-      require('trouble').setup({
-        position = 'bottom',           -- position of the list can be: bottom, top, left, right
-        height = 10,                   -- height of the trouble list when position is top or bottom
-        width = 50,                    -- width of the list when position is left or right
-        icons = true,                  -- use devicons for filenames
-        mode = 'document_diagnostics', -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
-        fold_open = '',             -- icon used for open folds
-        fold_closed = '',           -- icon used for closed folds
-        group = true,                  -- group results by file
-        padding = true,                -- add an extra new line on top of the list
-        action_keys = {
-          -- key mappings for actions in the trouble list
-          -- map to {} to remove a mapping, for example:
-          -- close = {},
-          close = 'q',                     -- close the list
-          cancel = '<esc>',                -- cancel the preview and get back to your last window / buffer / cursor
-          refresh = 'r',                   -- manually refresh
-          jump = { '<cr>', '<tab>' },      -- jump to the diagnostic or open / close folds
-          open_split = { '<c-x>' },        -- open buffer in new split
-          open_vsplit = { '<c-v>' },       -- open buffer in new vsplit
-          open_tab = { '<c-t>' },          -- open buffer in new tab
-          jump_close = { 'o' },            -- jump to the diagnostic and close the list
-          toggle_mode = 'm',               -- toggle between "workspace" and "document" diagnostics mode
-          toggle_preview = 'P',            -- toggle auto_preview
-          hover = 'K',                     -- opens a small popup with the full multiline message
-          preview = 'p',                   -- preview the diagnostic location
-          close_folds = { 'zM', 'zm' },    -- close all folds
-          open_folds = { 'zR', 'zr' },     -- open all folds
-          toggle_fold = { 'zA', 'za' },    -- toggle fold of current file
-          previous = 'k',                  -- preview item
-          next = 'j',                      -- next item
-        },
-        indent_lines = true,               -- add an indent guide below the fold icons
-        auto_open = false,                 -- automatically open the list when you have diagnostics
-        auto_close = false,                -- automatically close the list when you have no diagnostics
-        auto_preview = true,               -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
-        auto_fold = false,                 -- automatically fold a file trouble list at creation
-        auto_jump = { 'lsp_definitions' }, -- for the given modes, automatically jump if there is only a single result
-        signs = {
-          -- icons / text used for a diagnostic
-          error = icons.errorOutline,
-          warning = icons.warningTriangleNoBg,
-          hint = icons.lightbulbOutline,
-          information = icons.infoOutline,
-        },
-        use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
-      })
-    end,
-    init = function()
-      require('userlib.legendary').pre_hook('setup_trouble_lg', function(lg)
-        lg.commands({
-          -- troubles.
-          {
-            ':TodoTrouble',
-            description = 'Show todo in trouble',
-          },
-          {
-            [[:exe "TodoTrouble cwd=" . expand("%:p:h")]],
-            description = 'Show todo in trouble within current file directory',
-          },
-        })
-      end)
     end,
   },
 
