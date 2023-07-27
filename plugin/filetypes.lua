2adc4ed3f61e7e0206acb67e46b78408dbbe18ea
towry <towry@users.noreply.github.com>
2023-07-27 22:11:45 +0800

    up

--- plugin/filetypes.lua
+++ plugin/filetypes.lua
@@ -2,11 +2,13 @@ vim.filetype.add({
   extension = {
     ['es6'] = 'javascript',
     ['code-snippets'] = 'json',
-    ['handlebars'] = 'html'
+    ['handlebars'] = 'html',
+    ['tigrc'] = 'bash'
   },
   filename = {
     ['.envrc'] = 'bash',
     ['Brewfile'] = 'brewfile',
+    ['config'] = 'bash',
   },
   pattern = {
     ['.*ignore$'] = "gitignore",
