91747f15ff7182b79a162734732bb5209399b658
towry <towry@users.noreply.github.com>
2023-08-01 09:11:16 +0800

    up

--- after/ftplugin/oil.lua
+++ after/ftplugin/oil.lua
@@ -1,4 +1,11 @@
 vim.b.minianimate_disable = true
 vim.opt.spell = false
 
--- local bufnr = vim.api.nvim_get_current_buf()
+local bufnr = vim.api.nvim_get_current_buf()
+local set = require('userlib.runtime.keymap').map_buf_thunk(bufnr)
+
+set('n', '<S-q>', function()
+  require('oil').close()
+end, {
+  desc = 'Close oil',
+})
