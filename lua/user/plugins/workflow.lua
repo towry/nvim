308671913025415490b9503028d1226d304aefea
towry <towry@users.noreply.github.com>
2023-08-03 15:25:24 +0800

    Add bufjump

--- lua/user/plugins/workflow.lua
+++ lua/user/plugins/workflow.lua
@@ -161,6 +161,7 @@ plug({
   ----- grapple and portal
   {
     'cbochs/portal.nvim',
+    enabled = false,
     cmd = { 'Portal' },
     keys = {
 
@@ -478,5 +479,17 @@ plug({
     init = function()
       vim.g.harpoon_log_level = 'warn'
     end,
+  },
+  {
+    'kwkarlwang/bufjump.nvim',
+    keys = {
+      '<D-i>',
+      '<D-o>',
+    },
+    opts = {
+      forward = '<D-i>',
+      backward = '<D-o>',
+      on_success = nil,
+    }
   }
 })
