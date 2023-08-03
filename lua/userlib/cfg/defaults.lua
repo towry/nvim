4ce5dbc864b76383b5fc80727b2c1ea556018947
towry <towry@users.noreply.github.com>
2023-08-03 12:50:35 +0800

    Fix lsp log info level

--- lua/userlib/cfg/defaults.lua
+++ lua/userlib/cfg/defaults.lua
@@ -106,7 +106,7 @@ return {
   lang__treesitter_plugin_refactor = true,
   lang__treesitter_plugin_textobjects_move = true,
   lang__treesitter_plugin_textsubjects = true,
-  lsp__log_level = "INFO",
+  lsp__log_level = "ERROR",
   lsp__enable_servers = {
     "tailwindcss",
     "cssls",
