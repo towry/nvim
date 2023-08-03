bc1735ef7cf82724849b79881bff63224308721d
towry <towry@users.noreply.github.com>
2023-08-03 12:25:38 +0800

    up

--- lua/userlib/lsp/cfg/capbilities.lua
+++ lua/userlib/lsp/cfg/capbilities.lua
@@ -1,5 +1,6 @@
+---@param default_capabilities? table
 return function(default_capabilities)
-  local capabilities = default_capabilities or {}
+  local capabilities = default_capabilities or require('cmp_nvim_lsp').default_capabilities()
 
   capabilities.textDocument.foldingRange = {
     dynamicRegistration = false,
