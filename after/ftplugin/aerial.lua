f1691e7bf7750d6d8506a33e1eb337077fcae819
towry <towry@users.noreply.github.com>
2023-08-15 09:45:34 +0800

    fix ftplugin

--- after/ftplugin/aerial.lua
+++ after/ftplugin/aerial.lua
@@ -1,3 +1,9 @@
-vim.cmd([[setlocal nospell]])
-vim.cmd([[setlocal foldcolumn=0]])
-vim.cmd([[setlocal nohlsearch]])
+local api = vim.api
+local winid = vim.api.nvim_get_current_win()
+
+api.nvim_set_option_value('spell', false, {
+  win = winid,
+})
+api.nvim_set_option_value('foldcolumn', '0', {
+  win = winid,
+})
