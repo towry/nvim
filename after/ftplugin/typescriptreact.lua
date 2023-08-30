6f30a557f5bf2fc63fc22a1b3991900607368d23
towry <towry@users.noreply.github.com>
2023-08-30 13:35:37 +0800

    add ts for tsx fix

--- after/ftplugin/typescriptreact.lua
+++ after/ftplugin/typescriptreact.lua
@@ -1,1 +1,8 @@
 vim.treesitter.language.register('tsx', 'typescriptreact')
+
+--- bugfix for ts
+vim.g.treesitter_start = vim.g.treesitter_start or vim.treesitter.start
+vim.treesitter.start = function(...)
+  vim.g.treesitter_start(...)
+  vim.treesitter.language.register('tsx', 'typescriptreact')
+end
