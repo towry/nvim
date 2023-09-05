f9ccf14b56e2cef056dae11db55b986fe091ec93
towry <towry@users.noreply.github.com>
2023-09-05 19:31:57 +0800

    up

--- after/ftplugin/harpoon.lua
+++ after/ftplugin/harpoon.lua
@@ -3,9 +3,14 @@
 for i = 1, 9 do
   local key = tostring(i)
   local index = i
-  vim.api.nvim_buf_set_keymap(0, "n", key, "<cmd>lua require('harpoon.ui').nav_file(" .. index .. ")<CR>",
-    { noremap = true, silent = true, nowait = true })
+  vim.api.nvim_buf_set_keymap(
+    0,
+    'n',
+    key,
+    "<cmd>lua require('harpoon.ui').nav_file(" .. index .. ')<CR>',
+    { noremap = true, silent = true, nowait = true }
+  )
 end
 -- move cursor to the 10th item.
-vim.api.nvim_buf_set_keymap(0, "n", "0", "gg10j",
-  { expr = true, noremap = true, silent = true, nowait = true })
+vim.api.nvim_buf_set_keymap(0, 'n', '0', 'gg10j', { expr = true, noremap = true, silent = true, nowait = true })
+vim.wo.cursorline = true
