63b72ba3c21ce200cedc3c292072d0e8c965cd57
towry <towry@users.noreply.github.com>
2023-09-28 09:55:26 +0800

    up plugins

--- lua/user/plugins/statusline.lua
+++ lua/user/plugins/statusline.lua
@@ -142,6 +142,9 @@ plug({
       winbar = {
         lualine_a = {
           {
+            function() return vim.fn.bufnr('%') end,
+          },
+          {
             'filename',
             file_status = true,
             path = 3,
