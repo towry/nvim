1c5379eb5fca940014490b06a29c0c75bf7e94f0
towry <towry@users.noreply.github.com>
2023-10-18 17:19:02 +0800

    up

--- lua/user/plugins/git.lua
+++ lua/user/plugins/git.lua
@@ -317,8 +317,8 @@ plug({
       default_commands = true,
       disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
       highlights = { -- They must have background color, otherwise the default color will be used
-        -- incoming = 'DiffText',
-        -- current = 'DiffAdd',
+        incoming = 'DiffAdd',
+        current = 'DiffAdd',
       },
     })
 
