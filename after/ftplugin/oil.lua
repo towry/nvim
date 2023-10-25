46eb90cdc701b8085359023ccff086e50a168484
towry <towry@users.noreply.github.com>
2023-10-25 09:13:39 +0800

    Add choose window to open for oil

--- after/ftplugin/oil.lua
+++ after/ftplugin/oil.lua
@@ -19,6 +19,67 @@ end, {
   desc = 'Close oil',
 })
 
+-- <C-o>
+set('n', '<C-o>', function()
+  local util = require('oil.util')
+  local cache = require('oil.cache')
+  local entry = require('oil').get_cursor_entry()
+  local bufname = vim.api.nvim_buf_get_name(0)
+  if entry.type ~= 'file' then return end
+  -- https://github.com/stevearc/oil.nvim/blob/4088efb8ff664b6f1624aab5dac6c3fe11d3962c/lua/oil/init.lua#L495C44-L495C49
+  if entry.id == nil or (entry.id and cache.get_parent_url(entry.id) ~= bufname) or (entry.parsed_name ~= entry.name) then
+    vim.notify("New or Moved or Renamed file, please save it first before open")
+    return
+  end
+  local scheme, dir = util.parse_url(bufname)
+  local child = dir .. entry.name
+  local url = scheme .. child
+  local adapter = util.get_adapter(0)
+  if not adapter then
+    vim.notify("Could not find adapter to current buffer")
+    return
+  end
+
+  local get_edit_path
+  if adapter.get_entry_path then
+    get_edit_path = function(edit_cb)
+      adapter.get_entry_path(url, entry, edit_cb)
+    end
+  else
+    get_edit_path = function(edit_cb)
+      adapter.normalize_url(url, edit_cb)
+    end
+  end
+
+  get_edit_path(function(normalized_url)
+    local filename = util.escape_filename(normalized_url)
+    local win = require("window-picker").pick_window({
+      autoselect_one = true,
+      -- hint = 'floating-big-letter',
+      include_current_win = true,
+    })
+    if not win then return end
+
+    require('oil').close()
+
+    vim.api.nvim_set_current_win(win)
+    vim.cmd({
+      cmd = 'edit',
+      args = { filename },
+      mods = {
+        vertical = false,
+        horizontal = false,
+        keepalt = true,
+        emsg_silent = true,
+      }
+    })
+  end)
+end, {
+  desc = 'Previous buffer',
+  nowait = true,
+  noremap = true,
+})
+
 set('n', 's', function()
   require('flash').jump({
     search = {
