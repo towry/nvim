3b7feb3acbcb2c0e495daee703e48eea0a642281
towry <towry@users.noreply.github.com>
2023-10-28 11:17:51 +0800

    use gruvbox

--- lua/user/plugins/theme.lua
+++ lua/user/plugins/theme.lua
@@ -24,66 +24,26 @@ plug({
 })
 
 
+-- @see https://github.com/ellisonleao/gruvbox.nvim
 plug({
-  'rebelot/kanagawa.nvim',
+  'ellisonleao/gruvbox.nvim',
   event = 'User LazyTheme',
-  enabled = string.match(vim.cfg.ui__theme_name, 'kanagawa') ~= nil,
+  priority = 1000,
+  lazy = not string.match(vim.cfg.ui__theme_name, 'gruvbox'),
+  cond = vim.cfg.ui__theme_name == 'gruvbox',
   opts = {
-    undercurl = true, -- enable undercurls
-    commentStyle = { italic = true },
-    functionStyle = { bold = true },
-    keywordStyle = { italic = true },
-    statementStyle = { bold = true },
-    typeStyle = { bold = true },
-    variablebuiltinStyle = { italic = true },
-    globalStatus = true,
-    overrides = function(colors)
-      return {
-        --- mini files.
-        MiniFilesTitleFocused = { fg = colors.palette.lotusWhite3, bg = colors.palette.waveRed, bold = true },
-        MiniFilesBorderModified = {
-          fg = colors.palette.samuraiRed,
-        },
-        --- window picker
-        WindowPickerStatusLine = { bg = colors.theme.syn.fun, fg = colors.theme.ui.bg_m3 },
-        WindowPickerStatusLineNC = { bg = colors.theme.syn.fun, fg = colors.theme.ui.bg_m3 },
-      }
-    end,
-    colors = {
-      palette = {
-        crystalBlue = '#a7d8de',
-        -- dragonBlack0 = "#191f24",
-        -- dragonBlack1 = "#1c2228",
-        -- dragonBlack2 = "#192024",
-        -- dragonBlack3 = "#1c2428",
-        -- dragonBlack4 = "#232c30",
-        -- dragonBlack5 = "#2b353b",
-        -- dragonBlack6 = "#3b464f",
-        -- dragonBlue2 = "#7b96a3",
-        -- winterBlue = "#223140",
-
-        --- https://coolors.co/f5f4f2-f8f7f6-f9f9f8-fafafa-f1f5f1-dee2de-d4d8d4-caceca-a2a6a3-212121
-        -- lotusWhite0 = "#e6eaed",
-        -- lotusWhite1 = "#e4e6eb",
-        -- lotusWhite2 = "##f0f2f5",
-        -- lotusWhite3 = "#ffffff",
-        -- lotusWhite4 = "#e6eaed",
-        -- lotusWhite5 = "#f6f8fa",
-      },
-      theme = {
-        all = {
-          ui = {
-            bg_gutter = 'none',
-          },
-        },
-      },
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = {
+      strings = true,
+      emphasis = true,
+      comments = true,
+      operators = false,
+      folds = true,
+      contrast = "soft",
     },
-    background = {
-      dark = 'wave',
-      -- dark = 'dragon',
-      light = 'lotus',
-    },
-  },
+  }
 })
 
 plug({
