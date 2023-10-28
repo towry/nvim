9aa25d32b43950ecc318cc63b176f8ef456cb676
towry <towry@users.noreply.github.com>
2023-10-28 14:33:13 +0800

    Add command

--- lua/userlib/keymaps/overseer.lua
+++ lua/userlib/keymaps/overseer.lua
@@ -9,6 +9,17 @@ M.attach = function(bufnr)
   set('n', '<localleader>oo', '<cmd>OverseerToggle<cr>', {
     desc = "Overseer toggle"
   })
+  set('n', '<localleader>oR', function()
+    local overseer = require("overseer")
+    local tasks = overseer.list_tasks({ recent_first = true })
+    if vim.tbl_isempty(tasks) then
+      vim.notify("No tasks found", vim.log.levels.WARN)
+    else
+      overseer.run_action(tasks[1], "restart")
+    end
+  end, {
+    desc = 'Overseer restart last task',
+  })
 end
 
 return M
