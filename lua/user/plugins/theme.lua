72cab520f4af0854763bedf5efa8b223a371d642
towry <towry@users.noreply.github.com>
2023-10-30 16:37:40 +0800

    change theme

--- lua/user/plugins/theme.lua
+++ lua/user/plugins/theme.lua
@@ -10,13 +10,13 @@ plug({
   init = function()
     vim.g.gruvbox_material_background = 'medium'
     vim.g.gruvbox_material_ui_contrast = 'low'
-    vim.g.gruvbox_material_better_performance = 0
+    vim.g.gruvbox_material_better_performance = 1
     vim.g.gruvbox_material_enable_italic = 1
     vim.g.gruvbox_material_disable_italic_comment = false
     vim.g.gruvbox_material_transparent_background = false
     vim.g.gruvbox_material_dim_inactive_windows = false
-    vim.g.gruvbox_material_sign_column_background = 'none' -- "none" | "grey"
-    vim.g.gruvbox_material_diagnostic_virtual_text = 'colored' -- "grey" | "colored"
+    vim.g.gruvbox_material_sign_column_background = 'none'  -- "none" | "grey"
+    vim.g.gruvbox_material_diagnostic_virtual_text = 'grey' -- "grey" | "colored"
     vim.g.gruvbox_material_diagnostic_text_highlight = 1
     vim.g.gruvbox_material_diagnostic_line_highlight = 1
     vim.g.gruvbox_material_current_word = 'underline'
