7e4a3cef86614f842b16868212d88e4c56ff45bf
towry <towry@users.noreply.github.com>
2023-11-17 09:32:01 +0800

    remove spider

--- lua/user/plugins/motion.lua
+++ lua/user/plugins/motion.lua
@@ -239,34 +239,4 @@ plug({
       'CmdlineEnter',
     },
   },
-
-  {
-    'chrisgrieser/nvim-spider',
-    keys = {
-      {
-        'w',
-        "<cmd>lua require('spider').motion('w')<CR>",
-        desc = "Spider-w",
-        mode = {
-          "n", "o", "x"
-        }
-      },
-      {
-        'e',
-        "<cmd>lua require('spider').motion('e')<CR>",
-        desc = "Spider-e",
-        mode = {
-          "n", "o", "x"
-        }
-      },
-      {
-        'b',
-        "<cmd>lua require('spider').motion('b')<CR>",
-        desc = "Spider-b",
-        mode = {
-          "n", "o", "x"
-        }
-      }
-    }
-  }
 })
