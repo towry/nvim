0cc6833bf1b4d3af1712dec9263b2794f488c921
towry <towry@users.noreply.github.com>
2023-11-21 20:57:14 +0800

    nicely cwd

--- lua/userlib/mini-clue/folder-action.lua
+++ lua/userlib/mini-clue/folder-action.lua
@@ -3,9 +3,10 @@ local M = {}
 
 M.open = function(new_cwd)
   new_cwd = require('userlib.runtime.path').remove_path_last_separator(new_cwd)
+  local nicely_cwd = require('userlib.runtime.path').home_to_tilde(new_cwd)
 
   require('userlib.mini-clue').shortly_open(function(set, unset)
-    set('n', '1', '<cmd>echo expand("%")<cr>', { desc = new_cwd })
+    set('n', '1', '<cmd>echo expand("%")<cr>', { desc = nicely_cwd })
     set('n', 'f', function()
       require('userlib.telescope.pickers').project_files({
         cwd = new_cwd,
