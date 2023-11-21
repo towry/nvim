f234ea270bc7f757e497db9cddab7766fc14f0e3
towry <towry@users.noreply.github.com>
2023-11-21 11:05:05 +0800

    fix ai

--- lua/user/plugins/motion.lua
+++ lua/user/plugins/motion.lua
@@ -33,10 +33,12 @@ plug({
     },
   },
   {
-    'echasnovski/mini.ai',
+    'pze/mini.ai',
     -- disabled due to not compatible with nvim-treesitter#1.0
-    enabled = false,
-    event = au.user_autocmds.FileOpenedAfter_User,
+    enabled = true,
+    dev = false,
+    -- event = au.user_autocmds.FileOpenedAfter_User,
+    event = { 'CursorHold' },
     opts = function()
       local ai = require('mini.ai')
       return {
@@ -46,57 +48,20 @@ plug({
           o = ai.gen_spec.treesitter({
             a = { '@block.outer', '@conditional.outer', '@loop.outer' },
             i = { '@block.inner', '@conditional.inner', '@loop.inner' },
-          }, {}),
-          f = ai.gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }, {}),
-          c = ai.gen_spec.treesitter({ a = '@class.outer', i = '@class.inner' }, {}),
+          }, {
+            use_nvim_treesitter = false,
+          }),
+          f = ai.gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }, {
+            use_nvim_treesitter = false,
+          }),
+          c = ai.gen_spec.treesitter({ a = '@class.outer', i = '@class.inner' }, {
+            use_nvim_treesitter = false
+          }),
         },
       }
     end,
     config = function(_, opts)
       require('mini.ai').setup(opts)
-      -- register all text objects with which-key
-      if require('userlib.runtime.utils').has_plugin('which-key.nvim') then
-        ---@type table<string, string|table>
-        local i = {
-          [' '] = 'Whitespace',
-          ['"'] = 'Balanced "',
-          ["'"] = "Balanced '",
-          ['`'] = 'Balanced `',
-          ['('] = 'Balanced (',
-          [')'] = 'Balanced ) including white-space',
-          ['>'] = 'Balanced > including white-space',
-          ['<lt>'] = 'Balanced <',
-          [']'] = 'Balanced ] including white-space',
-          ['['] = 'Balanced [',
-          ['}'] = 'Balanced } including white-space',
-          ['{'] = 'Balanced {',
-          ['?'] = 'User Prompt',
-          _ = 'Underscore',
-          a = 'Argument',
-          b = 'Balanced ), ], }',
-          c = 'Class',
-          f = 'Function',
-          o = 'Block, conditional, loop',
-          q = 'Quote `, ", \'',
-          t = 'Tag',
-        }
-        local a = vim.deepcopy(i)
-        for k, v in pairs(a) do
-          a[k] = v:gsub(' including.*', '')
-        end
-
-        local ic = vim.deepcopy(i)
-        local ac = vim.deepcopy(a)
-        for key, name in pairs({ n = 'Next', l = 'Last' }) do
-          i[key] = vim.tbl_extend('force', { name = 'Inside ' .. name .. ' textobject' }, ic)
-          a[key] = vim.tbl_extend('force', { name = 'Around ' .. name .. ' textobject' }, ac)
-        end
-        require('which-key').register({
-          mode = { 'o', 'x' },
-          i = i,
-          a = a,
-        })
-      end
     end,
   },
 
