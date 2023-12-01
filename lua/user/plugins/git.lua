4ad2c583dcbcaf115664b25f96b3ebffe29b2629
towry <towry@users.noreply.github.com>
2023-12-01 17:33:49 +0800

    add merges

--- lua/user/plugins/git.lua
+++ lua/user/plugins/git.lua
@@ -38,7 +38,7 @@ plug({
       {
         '<leader>gF',
         cmdstr(
-          [[Git log --max-count=100 --oneline --date=format:"%Y-%m-%d %H:%M" --pretty=format:"%h %ad: %s - %an" --no-merges -- %]]),
+          [[Git log --max-count=100 --oneline --date=format:"%Y-%m-%d %H:%M" --pretty=format:"%h %ad: %s - %an" -- %]]),
         desc = 'Git show current file history'
       },
       {
