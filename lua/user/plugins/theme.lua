fa25fea4a50bb433bdb7d7009aa195b3d7130985
towry <towry@users.noreply.github.com>
2023-12-08 09:44:46 +0800

    fix theme diff

--- lua/user/plugins/theme.lua
+++ lua/user/plugins/theme.lua
@@ -1,8 +1,9 @@
 local plug = require('userlib.runtime.pack').plug
 
 plug({
-  'daschw/leaf.nvim',
+  'pze/leaf.nvim',
   event = 'User LazyTheme',
+  dev = false,
   priority = 1000,
   lazy = not string.match(vim.cfg.ui__theme_name, 'leaf'),
   enabled = vim.cfg.ui__theme_name == 'leaf',
