94920242601301ca8d0153b4e855e3924460151d
towry <towry@users.noreply.github.com>
2023-12-12 13:30:23 +0800

    add keybind

--- lua/user/vscode/setup.lua
+++ lua/user/vscode/setup.lua
@@ -11,6 +11,19 @@ set({ 'x' }, '<leader>cf', _call('editor.action.formatSelection'), {
 set({ 'n' }, '<leader>cf', _call('prettier.forceFormatDocument'), {
   desc = 'format with prettier if prettier exists'
 })
+set({ 'n' }, 'KK', _call('editor.action.showDefinitionPreviewHover'), {
+  desc = 'Show Definition Preview Hover',
+})
+set({ 'n' }, '<leader>cp', _call('editor.action.peekDefinition'), {
+  desc = 'Peek Definition',
+})
+set({ 'n' }, '<leader>ch', _call('editor.showCallHierarchy'), {
+  desc = 'Peek call hierarchy',
+})
+set({ 'n' }, '<leader>ci', _call('editor.action.peekImplementation'), {
+  desc = 'Peek implementation',
+})
+--//
 set({ 'n' }, '<leader>ff', _call('workbench.action.quickOpen'), {
   desc = 'Open file finder',
 })
@@ -20,3 +33,24 @@ set({ 'n' }, '<leader>fs', _call('workbench.action.findInFiles'), {
 set({ 'n' }, '<leader>fg', _call('workbench.action.findInFiles'), {
   desc = 'Search in files',
 })
+--// finder
+set({ 'n' }, 'gh[', _call('workbench.action.editor.previousChange'), {
+  desc = 'Previous change',
+})
+set({ 'n' }, 'gh]', _call('workbench.action.editor.nextChange'), {
+  desc = 'Next change',
+})
+--// motion
+set({ 'n' }, 'gha', _call('git.stage'), {
+  desc = 'Stage change',
+})
+set({ 'n' }, 'ghs', _call('git.stage'), {
+  desc = 'Stage change',
+})
+set({ 'n' }, '<leader>gs', _call('git.openChange'), {
+  desc = 'Open git changes',
+})
+set({ 'n', }, 'ghb', _call('gitlens.toggleFileBlame'), {
+  desc = 'Toggle file blame',
+})
+
