9d0c323852ddef36f8b313b9a003be84bc10a063
Towry <towry@users.noreply.github.com>
2023-12-21 16:15:14 +0800

    add query to ts

--- after/queries/typescript/aerial.scm
+++ after/queries/typescript/aerial.scm
@@ -1,6 +1,38 @@
 ;; extends
 
 ((import_statement
-  (import_clause) @name (#set! @name "text" "imports"))+
+  (import_clause) @name (#set! @name "text" "IMPORTS"))+
   (#set! "kind" "Module")
+ ) @symbol
+
+((
+  (decorator
+    (call_expression
+      function: (identifier) @decorator
+      ))
+  (public_field_definition
+    name: (property_identifier) @name
+    type: (type_annotation))+)
+ (#eq? @decorator "Prop")
+ (#set! "kind" "Field")
+ (#gsub! @name "^(.+)$" "@Prop: %1")
+) @symbol
+
+;; match property 
+(public_field_definition
+  name: (property_identifier) @name
+  value: (_) @value
+  (#set! "kind" "Field")
+) @symbol
+
+;; match block of variables
+(((lexical_declaration
+  (variable_declarator
+    name: (identifier) @name
+    value: (_)
+  ) @name (#set! @name "text" "VARIABLES")
+)(
+ comment
+)*)+
+ (#set! "kind" "Struct")
  ) @symbol
