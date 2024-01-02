2682e6ba694f1c49dc449c5ed6e00e8759a65a03
Towry <towry@users.noreply.github.com>
2024-01-02 14:05:50 +0800

    add abbr

---
+++ after/plugin/abbr.lua
@@ -0,0 +1,14 @@
+local au = require('userlib.runtime.au')
+
+au.on_verylazy(function()
+  ---- create abbreviations for command line.
+  --- @param abbr string
+  --- @param expand string
+  local setca = function(abbr, expand)
+    vim.api.nvim_set_keymap('ca', abbr, expand, { expr = false })
+  end
+
+  setca('avo', 'vertical Oil')
+  setca('alp', 'Lazy profile')
+  setca('ad', 'Dispatch')
+end)
