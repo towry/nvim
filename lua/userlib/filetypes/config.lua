234a19d2a044c4516ef39e8b5107cc305abbb69a
Towry <towry@users.noreply.github.com>
2024-01-08 15:59:55 +0800

    fix(lsp): add eslint and null-ls to vue ft

--- lua/userlib/filetypes/config.lua
+++ lua/userlib/filetypes/config.lua
@@ -20,8 +20,7 @@ return {
     filetypes = vim.cfg.lsp__server_volar_takeover_mode
         and { 'vue', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' }
       or { 'vue' },
-    lspconfig = { 'volar' },
-    treesitter = { 'vue' },
+    lspconfig = { 'volar', 'null-ls', 'eslint' },
   },
   ['typescript'] = {
     filetypes = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' },
--- lua/userlib/lsp/servers/volar.lua
+++ lua/userlib/lsp/servers/volar.lua
@@ -21,22 +21,24 @@ M.settings = {
 
 --- https://github.com/volarjs/volar.js/blob/master/packages/language-server/lib/types.ts
 
-local utils = require('userlib.runtime.utils')
-local node_root = utils.get_root()
-local node_util = require('userlib.runtime.platform.nodejs')
+return function()
+  local utils = require('userlib.runtime.utils')
+  local node_root = utils.get_root()
+  local node_util = require('userlib.runtime.platform.nodejs')
 
-local fts = vim.cfg.lsp__server_volar_takeover_mode
-    and { 'vue', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' }
-  or { 'vue' }
-local tsdk_path = node_util.get_typescript_server_path(node_root)
+  local fts = vim.cfg.lsp__server_volar_takeover_mode
+      and { 'vue', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' }
+    or { 'vue' }
+  local tsdk_path = node_util.get_typescript_server_path(node_root)
 
-return vim.tbl_extend('force', {}, {
-  filetypes = fts,
-  commands = require('userlib.lsp.commands'),
-  settings = M.settings,
-  init_options = {
-    typescript = {
-      tsdk = tsdk_path,
+  return vim.tbl_extend('force', {}, {
+    filetypes = fts,
+    commands = require('userlib.lsp.commands'),
+    settings = M.settings,
+    init_options = {
+      typescript = {
+        tsdk = tsdk_path,
+      },
     },
-  },
-})
+  })
+end
