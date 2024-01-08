cc5470a8eb3778375eda2bb5c83813f28690f57b
Towry <towry@users.noreply.github.com>
2024-01-08 17:33:15 +0800

    chore: cap

--- lua/userlib/lsp/cfg/capbilities.lua
+++ lua/userlib/lsp/cfg/capbilities.lua
@@ -1,11 +1,31 @@
+local function add_snippet_compa(cap)
+  cap.textDocument.completion = {
+    completionItem = {
+      snippetSupport = true,
+      resolveSupport = {
+        properties = { 'edit', 'documentation', 'detail', 'additionalTextEdits' },
+      },
+    },
+    completionList = {
+      itemDefaults = {
+        'editRange',
+        'insertTextFormat',
+        'insertTextMode',
+        'data',
+      },
+    },
+  }
+  return cap
+end
+
 ---@param default_capabilities? table
 return function(default_capabilities)
   local capabilities = default_capabilities
   if not capabilities then
     if vim.cfg.edit__use_native_cmp then
       capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = add_snippet_compa(capabilities)
     else
-      print("---")
       capabilities = require('cmp_nvim_lsp').default_capabilities()
     end
   end
