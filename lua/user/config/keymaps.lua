bfc4c6ba232eaaa63bd10b80d2690fdf9e29a8db
Towry <towry@users.noreply.github.com>
2024-01-10 17:18:43 +0800

    fix: fix ai check

--- lua/user/config/keymaps.lua
+++ lua/user/config/keymaps.lua
@@ -354,7 +354,7 @@ local function setup_basic()
         -- trigger ai
         if vim.b._copilot then
           vim.fn.feedkeys(vim.fn['copilot#Suggest'](), 'i')
-        elseif vim.b._codeium_completions then
+        elseif vim.fn.exists('*codeium#Complete') == 1 then
           vim.fn.feedkeys(vim.fn['codeium#Complete'](), 'i')
         end
       end
