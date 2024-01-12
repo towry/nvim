720ec30c9a11906010e7147bbe83bdd882888aa3
Towry <towry@users.noreply.github.com>
2024-01-12 23:11:11 +0800

    disable visits on git commit

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,7 +1,4 @@
-local bufnr = vim.api.nvim_get_current_buf()
-local libutils = require('userlib.runtime.utils')
-local set = require('userlib.runtime.keymap').map_buf_thunk(bufnr)
-
 vim.api.nvim_buf_set_keymap(0, 'n', 'q', '<cmd>q<cr>', { silent = true, nowait = true, noremap = true })
 vim.b[0].autoformat_disable = true
 vim.opt_local.spell = true
+vim.b.minivisits_disable = true
