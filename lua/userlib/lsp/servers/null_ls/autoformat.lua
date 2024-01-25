02d4cbb6b3bd472701786ebe4de43d55fe7245d3
Towry <towry@users.noreply.github.com>
2024-01-25 15:05:05 +0800

    fix: do not format in merge

--- lua/userlib/lsp/servers/null_ls/autoformat.lua
+++ lua/userlib/lsp/servers/null_ls/autoformat.lua
@@ -5,8 +5,8 @@ local M = {}
 --- Is auto format is disabled
 function M.disabled(bufnr)
   -- do not format on git merge.
-  if vim.cfg.runtime__starts_as_gittool then
-    return
+  if vim.cfg.runtime__starts_as_gittool or vim.wo.diff then
+    return true
   end
   if (bufnr or bufnr == 0) and vim.b[bufnr].autoformat_disable then
     return true
