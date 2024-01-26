58246d6a30a700816b9d544758791d973776d530
Towry <towry@users.noreply.github.com>
2024-01-26 15:16:24 +0800

    chore: update doc

--- lua/user-notes.lua
+++ lua/user-notes.lua
@@ -10,6 +10,9 @@
 ---@mod user-notes.pattern Pattern
 ---@mod user-notes.pattern.lookahead-and-lookbehind Lookahead and lookbehind
 ---@brief [[
+---refs ~
+---- https://vim.fandom.com/wiki/Regex_lookahead_and_lookbehind
+---
 ---在搜索或者替换中，可以使用 lookahead /or lookbehind 辅助查找.
 ---比如如下内容:
 --->
@@ -19,7 +22,7 @@
 --->
 ---:s/:\s"\zs.\+\ze"/workspace:*
 ---<
----其中，'\zs' 表示前面的都不参与匹配结果, '\ze' 表示后面都不参与匹配结果。
+---其中，`\zs` 表示前面的都不参与匹配结果, `\ze` 表示后面都不参与匹配结果。
 ---@brief ]]
 
 local M = {}
