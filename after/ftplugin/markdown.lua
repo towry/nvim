a4dfef22e802817ddf415fd0e3ac88b078be217d
Towry <towry@users.noreply.github.com>
2024-02-21 17:41:07 +0800

    fix: conceallevel config

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,4 +1,5 @@
 -- https://github.com/echasnovski/nvim/blob/master/after/ftplugin/markdown.lua
 vim.cmd([[setlocal nospell]])
+vim.cmd([[setlocal conceallevel=0]])
 vim.cmd([[setlocal wrap]])
 vim.b.miniindentscope_disable = true
--- lua/user/config/options.lua
+++ lua/user/config/options.lua
@@ -43,8 +43,8 @@ function M.startup()
   o.autoindent = true --- Good auto indent
   o.backspace = { 'indent', 'eol', 'start' } --- Making sure backspace works
   o.backup = false --- Recommended by coc
-  o.conceallevel = 2 --- Show `` in markdown files
-  -- o.concealcursor = 'n'
+  o.conceallevel = 1 --- Show `` in markdown files
+  -- o.concealcursor = 'i'
   o.encoding = 'utf-8' --- The encoding displayed
   o.errorbells = false --- Disables sound effect for errors
   o.fileencoding = 'utf-8' --- The encoding written to file
