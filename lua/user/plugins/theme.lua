f7a46e9061563b04751b7ff0d66fdc8ae4f70027
Towry <towry@users.noreply.github.com>
2024-03-15 21:10:05 +0800

    update plugins

--- lua/user/plugins/theme.lua
+++ lua/user/plugins/theme.lua
@@ -1,39 +1,6 @@
 local plug = require('userlib.runtime.pack').plug
 
 plug({
-  -- 'ellisonleao/gruvbox.nvim',
-  'pze/gruvbox.nvim',
-  dev = false,
-  event = 'User LazyTheme',
-  priority = 1000,
-  enabled = vim.cfg.ui__theme_name == 'gruvbox',
-  opts = {
-    terminal_colors = true, -- add neovim terminal colors
-    undercurl = true,
-    underline = true,
-    bold = true,
-    italic = {
-      strings = true,
-      emphasis = true,
-      comments = true,
-      operators = false,
-      folds = true,
-    },
-    strikethrough = true,
-    invert_selection = false,
-    invert_signs = false,
-    invert_tabline = false,
-    invert_intend_guides = false,
-    inverse = true, -- invert background for search, diffs, statuslines and errors
-    contrast = '', -- can be "hard", "soft" or empty string
-    palette_overrides = {},
-    overrides = {},
-    dim_inactive = false,
-    transparent_mode = false,
-  },
-})
-
-plug({
   -- https://protesilaos.com/emacs/modus-themes-pictures
   'miikanissi/modus-themes.nvim',
   event = 'User LazyTheme',
