b54fb4738df3af5a46077b5fda11bbce0f199ab6
Towry <towry@users.noreply.github.com>
2024-03-17 19:02:46 +0800

    fix: change flash keys

--- lua/user/plugins/motion.lua
+++ lua/user/plugins/motion.lua
@@ -161,7 +161,7 @@ plug({
         desc = 'Flash jump to line',
       },
       {
-        '[s',
+        '<C-s><C-p>',
         mode = { 'n', 'o' },
         function()
           require('flash').jump({
@@ -174,7 +174,7 @@ plug({
         desc = 'Flash backward search',
       },
       {
-        ']s',
+        '<C-s><C-n>',
         mode = { 'n', 'o' },
         function()
           require('flash').jump({
@@ -202,34 +202,6 @@ plug({
         end,
         desc = 'Remote Flash',
       },
-      -- {
-      --   '<C-o>o',
-      --   mode = 'i',
-      --   desc = 'o after flash',
-      --   function()
-      --     require('userlib.workflow.flashs').jump_to_line({
-      --       action = function(match)
-      --         vim.api.nvim_set_current_win(match.win)
-      --         vim.api.nvim_win_set_cursor(match.win, match.pos)
-      --         vim.cmd('normal! o')
-      --       end,
-      --     })
-      --   end,
-      -- },
-      -- {
-      --   '<C-o>O',
-      --   mode = 'i',
-      --   desc = 'O after flash in insert mode',
-      --   function()
-      --     require('userlib.workflow.flashs').jump_to_line({
-      --       action = function(match)
-      --         vim.api.nvim_set_current_win(match.win)
-      --         vim.api.nvim_win_set_cursor(match.win, match.pos)
-      --         vim.cmd('normal! O')
-      --       end,
-      --     })
-      --   end,
-      -- },
       {
         '<C-s>v',
         mode = { 'i', 'n' },
