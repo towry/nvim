63ac9961336976ac3116d510e476ae729f0b717e
Towry <towry@users.noreply.github.com>
2024-04-03 11:50:20 +0800

    doc: add fixme

--- colors/flexoki.lua
+++ colors/flexoki.lua
@@ -3,6 +3,8 @@ local load = function()
   flexoki.colorscheme({ variant = vim.o.background or 'dark' })
 end
 
+-- FIXME: this autocmd will be called everytime other colorscheme changed.
+
 vim.api.nvim_create_augroup('load_flexoki', { clear = true })
 vim.api.nvim_create_autocmd('OptionSet', {
   group = 'load_flexoki',
