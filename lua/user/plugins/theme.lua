b6db158f332832596a3cb646bde011b338474cd0
Towry <towry@users.noreply.github.com>
2024-04-17 19:14:44 +0800

    up

--- lua/user/plugins/theme.lua
+++ lua/user/plugins/theme.lua
@@ -263,3 +263,31 @@ plug({
     })
   end,
 })
+
+plug({
+  'EdenEast/nightfox.nvim',
+  event = 'User LazyTheme',
+  priority = 1000,
+  enabled = string.match(vim.cfg.ui__theme_name, 'fox') ~= nil,
+  config = function()
+    -- https://github.com/EdenEast/nightfox.nvim?tab=readme-ov-file#configuration
+    require('nightfox').setup({
+      options = {
+        styles = {
+          comments = 'italic',
+          keywords = 'bold',
+          types = 'italic,bold',
+        },
+      },
+      palettes = {
+        all = {},
+      },
+      specs = {},
+      groups = {},
+    })
+  end,
+  init = function()
+    vim.g.nightfox_day = 'dayfox'
+    vim.g.nightfox_night = 'nordfox'
+  end,
+})
