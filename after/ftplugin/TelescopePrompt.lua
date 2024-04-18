3d645c9b0cdbf424309d1fc99886436c81e8ff57
Towry <towry@users.noreply.github.com>
2024-04-18 18:16:33 +0800

    update

--- after/ftplugin/TelescopePrompt.lua
+++ after/ftplugin/TelescopePrompt.lua
@@ -2,3 +2,4 @@ vim.bo.omnifunc = ''
 vim.bo.cfu = ''
 vim.bo.cpt = ''
 vim.b.minicompletion_disable = true
+vim.cmd('setlocal nocursorline')
--- lua/user/plugins/theme.lua
+++ lua/user/plugins/theme.lua
@@ -113,3 +113,68 @@ plug({
     })
   end,
 })
+
+plug({
+  'gbprod/nord.nvim',
+  event = 'User LazyTheme',
+  cond = vim.cfg.ui__theme_name:match('nord'),
+  config = function()
+    require('nord').setup({
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      transparent = false, -- Enable this to disable setting the background color
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+      diff = { mode = 'fg' }, -- enables/disables colorful backgrounds when used in diff mode. values : [bg|fg]
+      borders = true, -- Enable the border between verticaly split windows visible
+      errors = { mode = 'fg' }, -- Display mode for errors and diagnostics
+      -- values : [bg|fg|none]
+      search = { theme = 'vim' }, -- theme for highlighting search results
+      -- values : [vim|vscode]
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = false, bold = false },
+        keywords = { italic = false },
+        functions = { bold = true, italic = false },
+        variables = {},
+      },
+
+      --- You can override specific highlights to use other groups or a hex color
+      --- function will be called with all highlights and the colorScheme table
+      on_highlights = function(hl, c)
+        -- hl.NormalFloat = { link = 'Pmenu' }
+        -- hl.TelescopeNormal = { link = 'NormalFloat' }
+        -- hl.TelescopeBorder = { link = 'FloatBorder' }
+        -- hl.FloatBorder = { link = 'NormalFloat' }
+
+        hl.CocErrorSign = { link = 'DiagnosticError' }
+        hl.CocWarningSign = { link = 'DiagnosticWarn' }
+        hl.CocInfoSign = { link = 'DiagnosticInfo' }
+        hl.CocHintSign = { link = 'DiagnosticHint' }
+        hl.CocErrorFloat = { link = 'DiagnosticError' }
+        hl.CocWarningFloat = { link = 'DiagnosticWarn' }
+        hl.CocFloating = { link = 'NormalFloat' }
+        hl.CocInfoFloat = { link = 'DiagnosticInfo' }
+        hl.CocHintFloat = { link = 'DiagnosticHint' }
+        hl.CocDiagnosticsError = { link = 'DiagnosticError' }
+        hl.CocDiagnosticsWarning = { link = 'DiagnosticWarn' }
+        hl.CocDiagnosticsInfo = { link = 'DiagnosticInfo' }
+        hl.CocDiagnosticsHint = { link = 'DiagnosticHint' }
+        hl.CocSelectedText = { fg = c.snow_storm.origin }
+        hl.CocMenuSel = { link = 'PmenuSel' }
+        hl.CocCodeLens = { fg = c.polar_night.bright }
+        hl.CocInlayHint = { fg = c.polar_night.bright }
+        hl.CocInlayHintType = { link = 'CocInlayHint' }
+        hl.CocInlayHintParameter = { link = 'CocInlayHint' }
+        hl.CocErrorHighlight = { undercurl = true, sp = c.aurora.red }
+        hl.CocWarningHighlight = { sp = c.aurora.yellow, undercurl = true }
+        hl.CocInfoHighlight = { sp = c.aurora.green, undercurl = true }
+        hl.CocHintHighlight = { sp = c.aurora.orange, undercurl = true }
+        hl.TreesitterContextBottom = {
+          underline = true,
+          sp = c.polar_night.brightest,
+        }
+      end,
+    })
+  end,
+})
