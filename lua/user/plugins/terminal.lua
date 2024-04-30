8ead60a71a2b7c21d4c7902b7a99adae3357834a
Towry <towry@users.noreply.github.com>
2024-04-30 16:30:51 +0800

    fix: add doc

--- lua/user/plugins/terminal.lua
+++ lua/user/plugins/terminal.lua
@@ -70,6 +70,7 @@ plug({
         hide_numbers = true, -- hide the number column in toggleterm buffers
         -- this can affect fzf-lua terminal.
         highlights = highlights,
+        --- this option cause fzf-lua hi not working properly
         -- shade_filetypes = { 'none', 'fzf' },
         shade_terminals = true,
         shading_factor = 1, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
