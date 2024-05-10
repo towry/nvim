4caec0f05a31324805421413227af4d37eddabde
Towry <towry@users.noreply.github.com>
2024-05-10 17:09:58 +0800

    fix: altbuf

--- lua/userlib/runtime/buffer.lua
+++ lua/userlib/runtime/buffer.lua
@@ -350,6 +350,14 @@ M.edit_alt_buf = function()
   if not altnr or altnr < 1 then
     return
   end
+  if not vim.api.nvim_buf_is_loaded(altnr) then
+    -- buf is deleted but not wipped out
+    ---@diagnostic disable-next-line: cast-local-type
+    altnr = M.next_bufnr()
+  end
+  if not altnr then
+    return
+  end
   M.set_current_buffer_focus(altnr)
   print('#' .. altnr)
 end
