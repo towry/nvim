a740c3d1dc6c87fa90e264e72b0e9c503aa526a7
Towry Wang <towry@users.noreply.github.com>
2024-06-25 17:54:46 +0800

    add vtsls settings

--- lua/userlib/lsp/servers/vtsls.lua
+++ lua/userlib/lsp/servers/vtsls.lua
@@ -6,4 +6,62 @@ M.cmd = {
   '--stdio',
 }
 
+--- https://raw.githubusercontent.com/yioneko/vtsls/main/packages/service/configuration.schema.json
+local settings = {
+  typescript = {
+    tsserver = {
+      maxTsServerMemory = 1500,
+    },
+    preferences = {
+      importModuleSpecifierEnding = 'index',
+      importModuleSpecifier = 'relative',
+    },
+    inlayHints = {
+      includeInlayParameterNameHints = 'all',
+      includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+      includeInlayFunctionParameterTypeHints = true,
+      includeInlayVariableTypeHints = false,
+      includeInlayPropertyDeclarationTypeHints = true,
+      includeInlayFunctionLikeReturnTypeHints = false,
+      includeInlayEnumMemberValueHints = true,
+    },
+  },
+  javascript = {
+    preferences = {
+      importModuleSpecifierEnding = 'index',
+      importModuleSpecifier = 'relative',
+    },
+    inlayHints = {
+      includeInlayParameterNameHints = 'all',
+      includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+      includeInlayFunctionParameterTypeHints = true,
+      includeInlayVariableTypeHints = false,
+      includeInlayPropertyDeclarationTypeHints = true,
+      includeInlayFunctionLikeReturnTypeHints = false,
+      includeInlayEnumMemberValueHints = true,
+    },
+  },
+
+  vtsls = {
+    tsserver = {
+      globalPlugins = {
+        {
+          name = '@vue/typescript-plugin',
+          location = vim.cfg.lsp__vue_typescript_plugin,
+          languages = { 'javascript', 'typescript', 'vue' },
+        },
+      },
+    },
+    autoUseWorkspaceTsdk = true,
+    experimental = {
+      completion = {
+        --- Execute fuzzy match of completion items on server side. Enable this will help filter out useless completion items from tsserver.
+        enableServerSideFuzzyMatch = true,
+      },
+    },
+  },
+}
+
+M.settings = settings
+
 return M
