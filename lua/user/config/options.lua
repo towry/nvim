000e137b989d680bd78369752160f49281c9e30c
Towry Wang <towry@users.noreply.github.com>
2024-06-29 17:56:39 +0800

    temp fix

--- lua/user/config/options.lua
+++ lua/user/config/options.lua
@@ -3,6 +3,7 @@ local o = vim.opt
 local g = vim.g
 
 function M.startup()
+  o.shell = 'direnv exec / fish'
   o.winbar = ''
   o.autowrite = true
   o.startofline = false -- cursor start of line when scroll
